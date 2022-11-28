c6878651eba1e628837e6bd7fc8ec294b7aa2add
BB pezsgő <bazsi5fel2@gmail.com>
2022-11-28 18:22:57 +0100

    Create upload.bat

---
+++ upload.bat
@@ -0,0 +1,5 @@
+@echo off
+set from=".\out\make\squirrel.windows\x64\interpeter-debugger-1.0.0 Setup.exe"
+set to="C:\Users\bazsi\OneDrive\Download Content"
+echo Copy %from% to %to%
+copy %from% %to%
